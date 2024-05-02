4db8c0cd2882ef359161efd0b907b6bc2c202a97
sappang <dsappang@gmail.com>
2024-05-02 13:58:57 +0700

    selesai membuat create category

--- resources/views/backoffice/category/index.blade.php
+++ resources/views/backoffice/category/index.blade.php
@@ -3,32 +3,46 @@
 @section('content')
     <x-container>
         <div class="col-12 col-lg-8">
-        <x-card-action title="Daftar Kategori" url="{{route('backoffice.category.index')}}">
-            <x-table>
-            <thead>
-                <tr>
-                <th>#</th>
-                <th>Gambar</th>
-                <th>Nama Kategori</th>
-                <th>Aksi</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($categories as $i => $category)
-                <tr>
-                    <td>{{ $i + $categories->firstItem() }}</td>
-                    <td>
-                    <span class="avatar rounded avatar-md"
-                    style="background-image: url({{ $category->image }})"></span>
-                    </td>
-                    <td>{{ $category->name }}</td>
-                    <td></td>
-                </tr>
-                @endforeach
-            </tbody>
-            </x-table>
-        </x-card-action>
-        <div class="d-flex justify-content-end">{{ $categories->links() }}</div>
+            <x-card-action title="Daftar Kategori" url="{{route('backoffice.category.index')}}">
+                <x-table>
+                <thead>
+                    <tr>
+                    <th>#</th>
+                    <th>Gambar</th>
+                    <th>Nama Kategori</th>
+                    <th>Aksi</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($categories as $i => $category)
+                    <tr>
+                        <td>{{ $i + $categories->firstItem() }}</td>
+                        <td>
+                        <span class="avatar rounded avatar-md"
+                        style="background-image: url({{ $category->image }})"></span>
+                        </td>
+                        <td>{{ $category->name }}</td>
+                        <td></td>
+                    </tr>
+                    @endforeach
+                </tbody>
+                </x-table>
+            </x-card-action>
+            <div class="d-flex justify-content-end">{{ $categories->links() }}</div>
         </div>
+        @can('category-create')
+        <div class="col-12 col-lg-4">
+            <form action="{{ route('backoffice.category.store') }}" method="POST" enctype="multipart/form-data">
+            @csrf
+            <x-card title="Tambah Kategori" class="card-body">
+                <x-input title="Nama Kategori" name="name" type="text" placeholder="Masukan Nama Kategori"
+                value="{{ old('name') }}" />
+                <x-input title="Gambar Kategori" name="image" type="file" placeholder=""
+                value="{{ old('image') }}" />
+                <x-button-save title="Simpan" />
+            </x-card>
+            </form>
+        </div>
+        @endcan
     </x-container>
 @endsection
